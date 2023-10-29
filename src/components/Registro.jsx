dd5679974385fac9c6927a660ba8ab1765ac6eb1
gonzalo lavin <gonzalolavin99@gmail.com>
2023-10-29 03:38:19 -0300

    Iconos agreados como props

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import SocialButton from './SocialButton'; 
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
-import SocialButton from './SocialButton'
 const Registro = () => {
   return (
     <div>
-        <h1>Crea una cuenta</h1>
-        <div>
-        <SocialButton></SocialButton>
-        </div>
+      <h1>Crea una cuenta</h1>
+      <div>
+        <SocialButton
+          facebookIcon={<FaFacebook />}
+          githubIcon={<FaGithub />}
+          linkedinIcon={<FaLinkedin />}
+        />
+      </div>
     </div>
   )
 }
 
-export default Registro
+export default Registro;
--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -1,24 +1,15 @@
 import React from "react";
-import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
 class SocialButton extends React.Component {
   render() {
     return (
       <div className="redes">
-        <div className=" icono">
-          <FaFacebook />
-        </div>
-        <div className="icono">
-          <FaGithub />
-        </div>
-        <div className="icono"> 
-          <FaLinkedin />
-        </div>
+        <div className="icono">{this.props.facebookIcon}</div>
+        <div className="icono">{this.props.githubIcon}</div>
+        <div className="icono">{this.props.linkedinIcon}</div>
       </div>
     );
   }
 }
-
-<FaGithub />;
 
 export default SocialButton;
